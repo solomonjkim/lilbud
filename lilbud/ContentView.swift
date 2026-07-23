161fa7177818ad7612e0d582e25784d88cc3e089
Solomon Kim <solomonjasonkim@gmail.com>
2026-07-23 09:50:49 -0700

    Initial Commit

---
+++ lilbud/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lilbud
+//
+//  Created by Solomon Kim on 7/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lilbud/lilbudApp.swift
@@ -0,0 +1,17 @@
+//
+//  lilbudApp.swift
+//  lilbud
+//
+//  Created by Solomon Kim on 7/23/26.
+//
+
+import SwiftUI
+
+@main
+struct lilbudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
